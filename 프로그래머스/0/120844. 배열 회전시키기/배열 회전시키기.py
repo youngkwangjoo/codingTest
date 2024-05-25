341a787b88856362011b4458457e593ddd62be07
youngkwangjoo <164307740+youngkwangjoo@users.noreply.github.com>
2024-05-25 23:32:54 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,7 @@
+def solution(numbers, direction):
+    if direction == "right":
+        return [numbers[-1]] + numbers[:-1]
+    elif direction == "left":
+        return numbers[1:] + [numbers[0]]
+
+
