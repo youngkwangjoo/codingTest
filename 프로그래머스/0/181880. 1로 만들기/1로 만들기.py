18eeef490050a42aa9eaa1a781f40e121f27d1e1
YoungKwangJoo <164307740+youngkwangjoo@users.noreply.github.com>
2024-10-14 15:53:39 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        result = 0
+        while i > 1:
+            if i % 2 == 0:
+                i = i // 2
+            else:
+                i = (i-1) // 2
+            result += 1
+        answer += result
+    return answer
